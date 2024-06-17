d441ea9c7547bb55ccdd35a7ca36e3cb1e06e8e8
sssssqew <sunrise.in.my.dream@gmail.com>
2024-06-17 11:33:08 +0900

    added test functionality with vitest

---
+++ template/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+console.log('Loaded vitest config')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  esbuild: {
+    loader: 'jsx',
+    include: /(src)\/.*\.jsx?$/,
+    exclude: [],
+  },
+  plugins: [react()],
+  test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+    // hey! 👋 over here
+    globals: true,
+    setupFiles: './tests/setup.js', // assuming the test folder is in the root of our project
+  }
+})
