f5e6614210b9f17e93c8ee63eb8719dadf34d6ed
sssssqew <sunrise.in.my.dream@gmail.com>
2024-06-17 16:57:32 +0900

    bug fixed how to use environment variables

--- template/src/App.test.js
+++ template/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App'
 
 describe('App', () => {
   it('renders the App component', () => {
+    console.log(REACT_APP_BASE_URL)
     render(<App />)
     
     screen.debug(); // prints out the jsx in the App component unto the command line
